25e7ef0fe16d1068eadf17547cc4e8bee39e9702
Kaspar Bach Pedersen <kasparp@me.com>
2020-06-12 17:29:38 +0200

    make it work better

--- src/main/java/net/redirectme/squirrelacademy/marketobserver/services/CharacterService.java
+++ src/main/java/net/redirectme/squirrelacademy/marketobserver/services/CharacterService.java
@@ -3,10 +3,16 @@ package net.redirectme.squirrelacademy.marketobserver.services;
 import net.redirectme.squirrelacademy.marketobserver.entities.PublicInfo;
 import net.redirectme.squirrelacademy.marketobserver.repositories.PublicInfoRepository;
 import org.openapitools.client.api.CharacterApi;
+import org.openapitools.client.model.GetCharactersCharacterIdCorporationhistory200Ok;
 import org.openapitools.client.model.GetCharactersCharacterIdOk;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.Optional;
 
 /***
  *
@@ -25,20 +31,35 @@ public class CharacterService {
     @Autowired
     CharacterApi characterApi;
 
+    @Autowired
+    @Qualifier("noAuth")
+    CharacterApi noAuthCharacterApi;
+
     private PublicInfo getPublicInfo(int characterId) throws Exception {
         GetCharactersCharacterIdOk charactersCharacterId =
-                characterApi.getCharactersCharacterId(characterId, esidatasource, null);
+                characterApi.getCharactersCharacterId(characterId, esidatasource, null).block();
         return new PublicInfo(charactersCharacterId, characterId);
     }
 
     public PublicInfo getPublicInfoCached(int characterId) throws Exception {
-        PublicInfo publicInfo = publicInfoRepository.findByCharacterid(characterId);
-        if (publicInfo ==null) {
+        PublicInfo publicInfo = publicInfoRepository.findById((long) characterId).orElse(null);
+        if (publicInfo == null) {
             publicInfo = getPublicInfo(characterId);
             publicInfoRepository.save(publicInfo);
         }
         return publicInfo;
     }
 
+    /**
+     * TODO This function relies on that the most recent corp is the first one returned. This is not a good way.
+     */
+    public Integer getCurrentCorpId(int characterId) {
+        Flux<GetCharactersCharacterIdCorporationhistory200Ok> history = noAuthCharacterApi.getCharactersCharacterIdCorporationhistory(characterId, esidatasource, null);
+        List<GetCharactersCharacterIdCorporationhistory200Ok> corps = history.collectList().block();
+        if (corps != null && corps.size() > 0) {
+            return corps.get(0).getCorporationId();
+        }
+        return null;
+    }
 
 }
